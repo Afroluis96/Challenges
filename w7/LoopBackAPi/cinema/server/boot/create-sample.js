6971c838fe856528bb5f4c52f152eff5fa295c65
Luis Aparicio <luisaparicioj316@gmail.com>
2018-07-12 19:01:10 -0600

    jueves

---
+++ w7/LoopBackAPi/cinema/server/boot/create-sample.js
@@ -0,0 +1,23 @@
+module.exports = function(app) {
+   /* app.dataSources.mongodb.automigrate('rooms', function(err) {
+      if (err) throw err;
+  
+      app.models.rooms.create([{
+        roomName: 'Bel Cafe',
+        capacity: 30,
+        price: 4
+      }, {
+        roomName: 'Bel',
+        capacity: 30,
+        price: 4
+      }, {
+        roomName: ' Cafe',
+        capacity: 30,
+        price: 4
+      }], function(err, rooms) {
+        if (err) throw err;
+  
+        console.log('Models created: \n', rooms);
+      });
+    });*/
+  };
